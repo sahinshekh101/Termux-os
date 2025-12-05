6530f7880543104afa4038ab8a36056777cd339a
sahinshekh101 <sahinshekh101@gmail.com>
2025-12-05 22:51:04 +0600

    Add files via upload

--- os.sh
+++ os.sh
@@ -21,7 +21,7 @@ banner ( ) {
                   
                   
                   printf ""
-                  echo -e "\e[1;31m  [\e[32m√\e[31m] \e[1;91m by \e[1;36mRaj Aryan \e[93m/ \e[100;92mFacebook - SAHIN SHEKH (Anime Lover)\e[0m"
+                  echo -e "\e[1;31m  [\e[32m√\e[31m] \e[1;91m by \e[1;36mSAHIN SHEKH\e[93m/ \e[100;92mFacebook - SAHIN SHEKH (Anime Lover)\e[0m"
                   }
 
                   wr  ( )  {
